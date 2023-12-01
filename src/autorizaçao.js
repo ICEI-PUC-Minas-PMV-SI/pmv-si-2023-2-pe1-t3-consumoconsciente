00c1c981bd79801c38b28f11ca01d1975e270524
Sofia Pitta Sesso <sofit_sops@hotmail.com>
2023-11-30 22:54:09 -0300

    consertei responsividade do botão de entrar na tela de login

--- src/autorizaçao.js
+++ src/autorizaçao.js
@@ -1,8 +1,9 @@
 const isUserLogged = localStorage.getItem('userlogado')
 
+const dropDown = document.getElementById('dropdown')
 const buttons = document.querySelectorAll('a')
-const headerButtons = [buttons[1], buttons[2], buttons[3], buttons[4], buttons[6], buttons[7], buttons[8], buttons[9]]
-const loginButtons = [buttons[5], buttons[10]]
+const headerButtons = document.getElementById('nav-right-box')
+const loginButtons = [buttons[1], buttons[5]]
 
 window.addEventListener('pageshow', function () {
     if (!isUserLogged) {
@@ -10,7 +11,6 @@ window.addEventListener('pageshow', function () {
         const characterWherePathBegins = currentUrl.lastIndexOf("/")
         const urlWithoutPath = currentUrl.slice(0, characterWherePathBegins)
         const urlPath = currentUrl.slice(characterWherePathBegins + 1, currentUrl.length)
-        console.log(urlPath)
         const unloggedUserAllowedPaths = ['index.html', 'login.html', 'cadastro.html', 'dicas.html']
         if (unloggedUserAllowedPaths.indexOf(urlPath) === -1) {
             window.location.href = `${urlWithoutPath}/login.html`
@@ -19,9 +19,8 @@ window.addEventListener('pageshow', function () {
 })
 
 if (!isUserLogged) {
-    for (const button of headerButtons) {
-        button.style.display = "none"
-    }
+    dropDown.style.display = 'none'
+    headerButtons.style.display = "none"
 
     for (const button of loginButtons) {
         button.style.display = "inline"
