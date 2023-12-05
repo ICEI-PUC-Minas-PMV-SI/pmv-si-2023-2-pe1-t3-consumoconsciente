478ef51d62a0f4b0f63970cef06570290b8178c4
Eduardo Almeida <143141945+eduardosgalmeida@users.noreply.github.com>
2023-12-04 21:45:42 -0300

    Recovery Password
    Recovery Password

---
+++ src/RecoveryPassword.js
@@ -0,0 +1,12 @@
+
+const input = document.getElementById('recovery-input')
+
+document.getElementById('recovery').addEventListener('click', function(){
+    console.log('Deu certo')
+} )
+
+
+
+
+
+
